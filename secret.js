73f29b1c4882339bd94f0d353a1d52ba223855e6
Christian Hess <christianhess94@gmail.com>
2017-10-25 12:10:11 -0300

    second commit

--- secret.js
+++ secret.js
@@ -1,6 +1,6 @@
 module.exports = {
-    //database: 'mongodb://christian:tcc1@ds155424.mlab.com:55424/tcc1' || '',
-    database: 'mongodb://localhost/tccdb',
+    database: 'mongodb://christian:tcc1@ds155424.mlab.com:55424/tcc1' || '',
+    //database: 'mongodb://localhost/tccdb',
     port: process.env.PORT || 3000,
     secretKey: "Arash@$@!#@"
 }
